00e711f29b4d12ff9cc08a6714d88c45c5a5dc02
Natanael <natanaelgplay@gmail.com>
2022-12-24 18:00:48 -0300

    Update tigerVideo.sh

--- tigerVideo.sh
+++ tigerVideo.sh
@@ -13,17 +13,25 @@
   export XDG_CONFIG_HOME="${HOME}/.config"
 }
 
+[ -f "${XDG_CONFIG_HOME}/alerta_de_videos" ] && {
+  exit 
+}
+
 export GTK_THEME=Adwaita:dark
 
-yad \
+nao_mostrar_novamente=$(yad \
     --center \
     --borders=32 \
     --image=logoNew.png \
     --sticky --undecorated --skip-taskbar --no-focus --timeout=10 --fixed \
-    --text='<span size="large"><big><b>Novo por aqui ? </b></big>\nNa pasta de vídeos, existem vários \ntutoriais de uso do sistema.\n</span>' \
+    --text='<span size="large"><b>Novo por aqui ? </b>\nNa pasta de vídeos, existem vários tutoriais de uso do sistema.\n</span>' \
     --text-align="left" \
     --form --field="Não mostrar mais":CHK \n \
     --button=gtk-open:42 \
-    --button=gtk-close
+    --button=gtk-close)
+    
+[ "${nao_mostrar_novamente}" = "TRUE|" ] && {
+  touch "${XDG_CONFIG_HOME}/alerta_de_videos"
+}
 
 [ "${?}" = "42" ] && xdg-open $(xdg-user-dir VIDEOS)
